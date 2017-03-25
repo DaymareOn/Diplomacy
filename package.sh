96484c31a04e574d40fdaa61dbe047a85acc4dd0
day <11396847+DaymareOn@users.noreply.github.com>
2017-03-25 23:54:09 +0000

    Version 0.4 for manager

--- package.sh
+++ package.sh
@@ -4,4 +4,4 @@ cd oolite.oxp.Day.Diplomacy.oxp
 zip -r ../oolite.oxp.Day.Diplomacy.zip *
 cd ..
 mv oolite.oxp.Day.Diplomacy.zip oolite.oxp.Day.Diplomacy.oxz
-scp oolite.oxp.Day.Diplomacy.oxz pradier.info:/tmp/
+#scp oolite.oxp.Day.Diplomacy.oxz pradier.info:/tmp/
