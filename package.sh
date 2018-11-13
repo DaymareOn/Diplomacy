3210364e0f1e34da307cb66b22a450c929016cb3
daydreamingday <david@pradier.info>
2018-11-13 14:37:28 +0000

    Bugfix: manifest.plist for the oxz manager, using oxz from the sourceforge repository rather than from the old owncloud repository.

--- package.sh
+++ package.sh
@@ -4,4 +4,3 @@ cd oolite.oxp.Day.Diplomacy.oxp
 zip -r ../oolite.oxp.Day.Diplomacy.zip *
 cd ..
 mv oolite.oxp.Day.Diplomacy.zip oolite.oxp.Day.Diplomacy.oxz
-cp oolite.oxp.Day.Diplomacy.oxz /home/day/ownCloud/personnelDavid/
