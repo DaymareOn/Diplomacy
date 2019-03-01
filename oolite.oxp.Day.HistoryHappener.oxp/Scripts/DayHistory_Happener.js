0e27a4c94fa2b432d0cdc854ff0f50e18e0bc0de
DaymareOn <david@pradier.info>
2019-03-02 00:15:47 +0100

    History Happener Interface

---
+++ oolite.oxp.Day.HistoryHappener.oxp/Scripts/DayHistory_Happener.js
@@ -0,0 +1,69 @@
+"use strict";
+this.name = "DayDiplomacy_080_Happener";
+this.author = "David (Day) Pradier";
+this.copyright = "(C) 2017 David Pradier";
+this.licence = "CC-NC-by-SA 4.0";
+this.description = "This script scripts the Happener F4 interface, which lets the player advance History as if it had jumped. It is designed for debug.";
+
+this._F4InterfaceCallback = function (choice) {
+    this._resetLinks();
+    switch (choice) {
+        case "ADVANCE":
+            worldScripts.DayDiplomacy_000_Engine.shipExitedWitchspace();
+            break;
+        default: // "EXIT":
+    }
+};
+
+this._displayF4Interface = function () {
+    player.ship.hudHidden || (player.ship.hudHidden = true);
+
+    var opts = {
+        screenID: "DiplomacyHistoryHappenerScreenId",
+        title: "System history happener",
+        allowInterrupt: true,
+        exitScreen: "GUI_SCREEN_INTERFACES",
+        message: "You may advance one History turn",
+        choices:{
+            ADVANCE:"Avance History one turn",
+            EXIT:"Exit"
+        }
+    };
+    mission.runScreen(opts, this._F4InterfaceCallback.bind(this));
+};
+this._initF4Interface = function () {
+    player.ship.dockedStation.setInterface("DiplomacyHistoryHappener",
+        {
+            title: "System history happener",
+            category: "Diplomacy",
+            summary: "Make History advance one turn",
+            callback: this._displayF4Interface.bind(this)
+        });
+};
+
+/*************************** OXP public functions ********************************************************/
+/*************************** End OXP public functions ****************************************************/
+
+/*************************** Oolite events ***************************************************************/
+this.shipDockedWithStation = function (station) {
+    this._initF4Interface();
+};
+this.missionScreenEnded = function () {
+    player.ship.hudHidden = false;
+};
+
+this._startUp = function () {
+    // FIXME rassembler
+    worldScripts.XenonUI && worldScripts.XenonUI.$addMissionScreenException("DiplomacyHistoryHappenerScreenId");
+    worldScripts.XenonReduxUI && worldScripts.XenonReduxUI.$addMissionScreenException("DiplomacyHistoryHappenerScreenId");
+
+    this._initF4Interface();
+    delete this._startUp; // No need to startup twice
+};
+this.startUp = function () {
+    log("DayDiplomacyHappener.startup", "1");
+    worldScripts.DayDiplomacy_000_Engine.$subscribe(this.name);
+    log("DayDiplomacyHappener.startup", "2");
+    delete this.startUp; // No need to startup twice
+};
+/*************************** End Oolite events ***********************************************************/
